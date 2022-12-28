57d99a0cae0998573157cf9b8e8efab10fc28b82
Foltrex <fpm.chernenkAV@bsu.by>
2022-12-28 15:34:50 +0300

    Fix problems with entities

--- art/src/main/java/com/scnsoft/art/entity/Address.java
+++ art/src/main/java/com/scnsoft/art/entity/Address.java
@@ -2,11 +2,12 @@ package com.scnsoft.art.entity;
 
 import java.util.UUID;
 
-import org.hibernate.annotations.GenericGenerator;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Data;
 
@@ -15,9 +16,11 @@ import lombok.Data;
 @Builder
 public class Address {
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @GeneratedValue
     private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
     private City city;
     private String streetName;
     private Integer streetNumber;
--- art/src/main/java/com/scnsoft/art/entity/Art.java
+++ art/src/main/java/com/scnsoft/art/entity/Art.java
@@ -3,10 +3,13 @@ package com.scnsoft.art.entity;
 import java.util.UUID;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.ManyToAny;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Data;
 
@@ -20,5 +23,7 @@ public class Art {
     private UUID id;
     private String filename;
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "artist_id")
     private Artist artist;
 }
