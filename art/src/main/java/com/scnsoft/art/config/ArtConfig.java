4961fcd9597113a9b3659ded34aec52855b0ffb3
Foltrex <fpm.chernenkAV@bsu.by>
2023-01-03 21:56:56 +0300

    Delete extra config class

--- art/src/main/java/com/scnsoft/art/config/ArtConfig.java
+++ art/src/main/java/com/scnsoft/art/config/ArtConfig.java
@@ -1,14 +1,20 @@
 package com.scnsoft.art.config;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ArtConfig {
+public class ArtConfig implements WebMvcConfigurer  {
+    
+    @LoadBalanced
     @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
-        return restTemplateBuilder.build();
+    RestTemplate loadBalanced() {
+        return new RestTemplate();
     }
+
 }
