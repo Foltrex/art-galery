c3ef2cf24d4e0f8da0720e81a3e3ede9899910f5
Foltrex <fpm.chernenkAV@bsu.by>
2023-01-30 10:24:14 +0300

    Add user database initialization

--- user/src/main/resources/schema.sql
+++ user/src/main/resources/schema.sql
@@ -1,4 +1,9 @@
--- 
+-- !! For development !! Delete later
+DROP SCHEMA public CASCADE;
+
+CREATE SCHEMA public;
+
+-------------------Sequence ddl----------------------
 CREATE SEQUENCE IF NOT EXISTS role_id_seq 
     INCREMENT 1 
     START 1 
@@ -6,9 +11,12 @@ CREATE SEQUENCE IF NOT EXISTS role_id_seq
     MAXVALUE 9223372036854775807 
     CACHE 1;
 
--- 
+-------------------Extension ddl---------------------
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-------------------Tables ddl------------------------
 CREATE TABLE IF NOT EXISTS account (
-    id UUID NOT NULL PRIMARY KEY,
+    id UUID PRIMARY KEY  DEFAULT uuid_generate_v4 (),
     account_type VARCHAR(255),
     blocked_since TIMESTAMP WITHOUT TIME ZONE,
     email VARCHAR(255) UNIQUE,
@@ -17,13 +25,13 @@ CREATE TABLE IF NOT EXISTS account (
     password VARCHAR(255)
 );
 
+CREATE TABLE IF NOT EXISTS role (
+    id BIGINT NOT NULL PRIMARY KEY DEFAULT nextval('role_id_seq'),
+    name INTEGER NOT NULL UNIQUE
+);
+
 CREATE TABLE IF NOT EXISTS accounts_m2m_roles (
     account_id UUID NOT NULL REFERENCES account (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     role_id BIGINT NOT NULL REFERENCES role (id) ON UPDATE NO ACTION ON DELETE NO ACTION,
     PRIMARY KEY (account_id, role_id)
-);
-
-CREATE TABLE IF NOT EXISTS role (
-    id BIGINT NOT NULL PRIMARY KEY DEFAULT nextval('role_id_seq'),
-    name INTEGER NOT NULL UNIQUE
 );
