9594bdd834304fc8cd8da051a0eaf15ea023cdd6
maxim-semenko <maks.semenko@gmail.com>
2023-02-03 12:43:03 +0300

    add logout method

--- user/src/main/java/com/scnsoft/user/controller/AccountController.java
+++ user/src/main/java/com/scnsoft/user/controller/AccountController.java
@@ -1,23 +1,16 @@
 package com.scnsoft.user.controller;
 
 import com.scnsoft.user.dto.AccountDto;
-import com.scnsoft.user.dto.RepresentativeDto;
 import com.scnsoft.user.dto.mapper.impl.AccountMapper;
-import com.scnsoft.user.payload.RegisterRepresentativeRequest;
 import com.scnsoft.user.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 
 @RestController
 @RequestMapping("accounts")
--- user/src/main/java/com/scnsoft/user/controller/AuthController.java
+++ user/src/main/java/com/scnsoft/user/controller/AuthController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,6 +42,12 @@ public class AuthController {
     public ResponseEntity<RepresentativeDto> registerRepresentative(
             @Valid @RequestBody RegisterRepresentativeRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(authService.registerRepresentative(request));
+    }
+
+    @GetMapping("/logout")
+    @PreAuthorize("isAuthenticated()")
+    public void logout() {
+        authService.logout();
     }
 
 }
