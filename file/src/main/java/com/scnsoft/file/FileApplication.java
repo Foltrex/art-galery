626357c9fe4623eec2a685ccb30eb251e74f7ba1
rs <rs@rs.com>
2023-03-23 16:27:13 +0400

    test

--- file/src/main/java/com/scnsoft/file/FileApplication.java
+++ file/src/main/java/com/scnsoft/file/FileApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 public class FileApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(FileApplication.class, args);
     }
