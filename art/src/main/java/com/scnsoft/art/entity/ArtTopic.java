7c7b97b43c30a36632b9269951a9ed24aebd5b85
maxim-semenko <maks.semenko@gmail.com>
2023-03-27 15:53:58 +0300

    add arts entities

---
+++ art/src/main/java/com/scnsoft/art/entity/ArtTopic.java
@@ -0,0 +1,29 @@
+package com.scnsoft.art.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "art_topic")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ArtTopic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String label;
+
+    private Boolean verified;
+}
---
+++ art/src/main/java/com/scnsoft/art/entity/model/Option.java
@@ -0,0 +1,8 @@
+package com.scnsoft.art.entity.model;
+
+public interface Option {
+    int getId();
+
+    String getLabel();
+
+}
