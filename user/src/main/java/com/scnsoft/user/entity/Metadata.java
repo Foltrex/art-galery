286e4748b830f94039a42a0aa384d7260e9bcb96
maxim-semenko <maks.semenko@gmail.com>
2023-03-28 14:19:52 +0300

    remade account_metadata

--- user/src/main/java/com/scnsoft/user/entity/Metadata.java
+++ user/src/main/java/com/scnsoft/user/entity/Metadata.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -23,7 +24,7 @@ import java.util.UUID;
 public class Metadata {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private UUID id;
 
     @ManyToOne
